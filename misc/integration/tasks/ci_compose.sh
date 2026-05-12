822dd0f5303988afc3259be1a96d1852f23424e6
MolassesLover <mail@molasses.love>
2026-05-12 22:47:33 +0200

    Fix oversight in CI entrypoint

--- misc/integration/tasks/ci_compose.sh
+++ misc/integration/tasks/ci_compose.sh
@@ -3,20 +3,21 @@
 set -euxo pipefail
 
 SOURCE_ROOT="$PWD"
+INSTALL_DIRECTORY="$SOURCE_ROOT/installdir"
 CONTAINER_DIRECTORY="misc/containers/$1"
 
 if [ ! -d "$CONTAINER_DIRECTORY" ]; then
 	exit 1
 fi
 
-mkdir installdir
+mkdir "$INSTALL_DIRECTORY"
 
 cd "$CONTAINER_DIRECTORY"
 docker compose up --build --force-recreate
 
 cd "$SOURCE_ROOT"
 
-if [ -z "$(ls -A "$CONTAINER_DIRECTORY")" ]; then
-    echo ":: $CONTAINER_DIRECTORY is empty!"
+if [ -z "$(ls -A "$INSTALL_DIRECTORY")" ]; then
+	echo ":: $INSTALL_DIRECTORY is empty!"
 	exit 1
 fi
