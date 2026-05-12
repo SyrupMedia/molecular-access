d68f4dbfcac5bb9742dfd0e0f6da81393921d4e9
MolassesLover <mail@molasses.love>
2026-05-12 20:15:44 +0200

    Add support for native libraries and packaged `libipc`

--- molaccess.nix
+++ molaccess.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchgit, cmake, ninja, python3, swig }:
+{ lib, stdenv, fetchgit, fetchFromGitHub, cmake, ninja, python3, swig }:
 stdenv.mkDerivation {
   name = "molaccess";
 
@@ -7,5 +7,26 @@ stdenv.mkDerivation {
   nativeBuildInputs = [ cmake ninja swig ];
 
   # to do cross compilation python will need to be here if I understand correctly, since it is being linked against
-  buildInputs = [ python3 ];
+  buildInputs = [ 
+    (
+      # Build libipc
+      stdenv.mkDerivation {
+        name = "cpp-ipc";
+        src = fetchFromGitHub {
+          owner = "mutouyun";
+          repo = "cpp-ipc";
+          rev = "a0c7725a1441d18bc768d748a93e512a0fa7ab52";
+          sha256 = "sha256-W8tWjc1YRUteYlLKpsl8XUjRCuMKsz9VBiVcexQj9BY=";
+        };
+
+        nativeBuildInputs = [ cmake ninja ];
+      }
+    )
+
+    python3   
+  ];
+
+  cmakeFlags =  [
+    "-D MOLACCESS_NATIVE_LIBRARIES=true"
+  ];
 }
